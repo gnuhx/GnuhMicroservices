28e3735827aee3125d4bd316f75014e1bd107080
Gnuh Le <huyhunglenguyen@gmail.com>
2024-06-26 00:59:49 +0700

    46. Develop Feature Handler class with CQRS and MediatR

--- gnuh-microservice/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ gnuh-microservice/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,6 +1,17 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    :IRequest<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler :IRequestHandler<CreateProductCommand,CreateProductResult>
 {
-    public class CreateProductHandler
+    public Task<CreateProductResult> Handle(CreateProductCommand cmd, CancellationToken cancellationToken)
     {
+        //Business Logic to create Product
+        throw new NotImplementedException();
     }
 }
