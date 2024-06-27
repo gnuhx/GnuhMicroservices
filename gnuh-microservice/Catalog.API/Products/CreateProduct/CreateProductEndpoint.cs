c728babe08bf576406ebb103413bb55180656443
Gnuh Le <huyhunglenguyen@gmail.com>
2024-06-28 00:35:20 +0700

    52. Develop POST Endpoint with Carter implements ICarterModule for Minimal Apis

--- gnuh-microservice/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ gnuh-microservice/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,4 +1,6 @@
 ﻿using Carter;
+using Mapster;
+using MediatR;
 
 namespace Catalog.API.Products.CreateProduct;
 
@@ -10,6 +12,25 @@ public class CreateProductEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        throw new NotImplementedException();
+        app.MapPost("/products",
+        async (
+            CreateProductRequest request, ISender sender) 
+            =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<CreateProductResponse>();
+
+                return Results.Created($"/products/{response.Id}", response);
+
+            }
+        )
+        .WithName("CreateProduct")
+        .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create Product")
+        .WithDescription("Create Product");
     }
 }
