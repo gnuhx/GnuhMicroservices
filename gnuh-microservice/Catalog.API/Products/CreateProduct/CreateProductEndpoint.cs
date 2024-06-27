bcbcc6b4378d43c3d58e505152fddf3a5da8b906
Gnuh Le <huyhunglenguyen@gmail.com>
2024-06-28 00:16:03 +0700

    50. Develop Create Product Endpoint with Minimal Apis and Carter

--- gnuh-microservice/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ gnuh-microservice/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,5 +1,15 @@
-﻿namespace Catalog.API.Products.CreateProduct;
+﻿using Carter;
 
-public class CreateProductEndpoint
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+
+public record CreateProductResponse(Guid Id);
+
+public class CreateProductEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        throw new NotImplementedException();
+    }
 }
